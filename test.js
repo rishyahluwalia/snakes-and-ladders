1c66634c9e402449edd22433105863c60e3eb03e
BuildTools <unconfigured@null.spigotmc.org>
2021-08-13 12:24:33 +1000

    created mini testing framework

--- test.js
+++ test.js
@@ -5,76 +5,56 @@ const {
   calculateNewPosition,
 } = require("./game.js");
 
-console.log(
-  "checkSnake() corectly determines if the specified position is a snake: ",
-  [
+const describe = (description, test) => {
+  console.log(`\n${description}:`);
+  test();
+};
+const it = (description, conditions) => {
+  const passed = conditions.every((test) => !!test);
+  console.log(`${description}: ${passed ? "PASSED" : "FAILED"}`);
+};
+
+describe("checkSnake()", () => {
+  it("corectly determines if the specified position is a snake", [
     checkSnake(0) === false,
     checkSnake(9) === true,
     checkSnake(15) === false,
     checkSnake(18) === true,
     checkSnake(27) === true,
     checkSnake(100) === false,
-  ].every((val) => val === true)
-    ? "PASSED"
-    : "FAILED"
-);
+  ]);
+});
 
-console.log(
-  "checkLadder() corectly determines if the specified position is a ladder: ",
-  [
+describe("checkLadder()", () => {
+  it("corectly determines if the specified position is a ladder", [
     checkLadder(0) === false,
     checkLadder(10) === false,
     checkLadder(25) === true,
     checkLadder(40) === false,
     checkLadder(55) === true,
     checkLadder(100) === false,
-  ].every((val) => val === true)
-    ? "PASSED"
-    : "FAILED"
-);
+  ]);
+});
 
-console.log(
-  "checkOverflowNearEnd() corectly determines if the specified position went beyond 100: ",
-  [
+describe("checkOverflowNearEnd()", () => {
+  it("corectly determines if the specified position went beyond 100", [
     checkOverflowNearEnd(0) === false,
     checkOverflowNearEnd(10) === false,
     checkOverflowNearEnd(91) === false,
     checkOverflowNearEnd(99) === false,
     checkOverflowNearEnd(100) === false,
     checkOverflowNearEnd(105) === true,
-  ].every((val) => val === true)
-    ? "PASSED"
-    : "FAILED"
-);
+  ]);
+});
 
-const snakePos = {
-  currPos: 7,
-  proposedPos: 9,
-};
+describe("calculateNewPosition()", () => {
+  const snakePos = { currPos: 7, proposedPos: 9 };
+  const ladderPos = { currPos: 20, proposedPos: 25 };
+  const overflowPos = { currPos: 96, proposedPos: 102 };
+  const normalPos = { currPos: 70, proposedPos: 76 };
 
-const ladderPos = {
-  currPos: 20,
-  proposedPos: 25,
-};
-
-const overflowPos = {
-  currPos: 96,
-  proposedPos: 102,
-};
-
-const normalPos = {
-  currPos: 70,
-  proposedPos: 76,
-};
-
-console.log(
-  "calculateNewPosition() corectly calculates the next position for the player: ",
-  [
-    calculateNewPosition(snakePos) === 6,
-    calculateNewPosition(ladderPos) === 35,
-    calculateNewPosition(overflowPos) === 96,
-    calculateNewPosition(normalPos) === 76,
-  ].every((val) => val === true)
-    ? "PASSED"
-    : "FAILED"
-);
+  it("calculates the next position for the player with a snake position", [ calculateNewPosition(snakePos) === 6 ]);
+  it("calculates the next position for the player with a ladder position", [ calculateNewPosition(ladderPos) === 35 ]);
+  it("calculates the next position for the player with an overflow position", [ calculateNewPosition(overflowPos) === 96 ]);
+  it("calculates the next position for the player with a normal position", [ calculateNewPosition(normalPos) === 76 ]);
+});
